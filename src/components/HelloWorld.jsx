272573f7da2d5711871ac6b28476685bf0bac5c9
John Tawiah <johnbaktawiah@gmail.com>
2022-03-09 09:51:54 +0000

    feat: added react to astro

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HelloWorld() {
+  return (
+    <div>
+      <div>Hello World</div>
+      <p>The man in the mirror is staring</p>
+    </div>
+  );
+}
+
+export default HelloWorld;
